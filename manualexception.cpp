9f0fd6982740eff260b94b92fa8b6cc7d113ac57
abil0 <hoenabil6@gmail.com>
2025-06-10 15:16:21 +0700

    tahap 26

--- manualexception.cpp
+++ manualexception.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main() {
     try {
-        cout
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        
     }
 }
