a8818c9f4828ffd65361159ffc24a0030e8bfddb
abil0 <hoenabil6@gmail.com>
2025-06-10 15:06:31 +0700

    tahap 22

--- exeptionstandard.cpp
+++ exeptionstandard.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     cout << "awal program" << endl;
     try{
-        array<int
+        array<int, 3> data = { 10, 20, 30};
+        cout << data.at(5)<<endl;
     }
 }
